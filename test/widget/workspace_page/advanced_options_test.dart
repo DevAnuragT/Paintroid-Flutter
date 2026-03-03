fe38245f126cbfdb79e2b2e67e451b4b5ab9c836
DevAnuragT <anuragthakur2102@gmail.com>
2026-03-04 00:33:21 +0530

    test(widget): add Advanced Options dialog flow test

---
+++ test/widget/workspace_page/advanced_options_test.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:paintroid/core/localization/app_localizations.dart';
+import 'package:paintroid/ui/pages/workspace_page/workspace_page.dart';
+import 'package:paintroid/ui/theme/theme.dart';
+
+void main() {
+  late Widget sut;
+
+  setUp(() {
+    final lightTheme = LightPaintroidThemeData();
+    final darkTheme = DarkPaintroidThemeData();
+
+    sut = ProviderScope(
+      child: PaintroidTheme(
+        lightTheme: lightTheme,
+        darkTheme: darkTheme,
+        child: MaterialApp(
+          theme: lightTheme.materialThemeData,
+          darkTheme: darkTheme.materialThemeData,
+          home: const WorkspacePage(),
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+          ],
+        ),
+      ),
+    );
+  });
+
+  testWidgets('[ADVANCED_OPTIONS]: opens dialog, defaults off, toggles and confirms',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(sut);
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byIcon(Icons.more_vert));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Advanced Options'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Advanced Options'), findsAtLeastNWidgets(1));
+    expect(find.text('Antialiasing'), findsOneWidget);
+    expect(find.text('Smoothing'), findsOneWidget);
+
+    final switches = tester.widgetList<Switch>(find.byType(Switch)).toList();
+    expect(switches.length, 2);
+    expect(switches[0].value, isFalse);
+    expect(switches[1].value, isFalse);
+
+    await tester.tap(find.text('Antialiasing'));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('OK'));
+    await tester.pumpAndSettle();
+  });
+}
